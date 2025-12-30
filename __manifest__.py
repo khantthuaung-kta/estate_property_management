d7096b8868e0ede98c55141c221811609e91d334
Khant Thu Aung <mr.khantthuaung7@gmail.com>
2025-12-30 11:06:07 +0630

    change manifest

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,6 @@
     """,
 
     'author': "Khant Thu Aung",
-    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
